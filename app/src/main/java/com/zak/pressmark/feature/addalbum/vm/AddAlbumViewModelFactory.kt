cc8934a62f4b3b0f220e3d5d108be250c1f383b5
Zak <zrfman91@gmail.com>
2026-01-18 23:33:32 -0600

    Inject ReleaseRepository into AddAlbumViewModelFactory

--- app/src/main/java/com/zak/pressmark/feature/addalbum/vm/AddAlbumViewModelFactory.kt
+++ app/src/main/java/com/zak/pressmark/feature/addalbum/vm/AddAlbumViewModelFactory.kt
@@ -1,7 +1,9 @@
+// file: app/src/main/java/com/zak/pressmark/feature/addalbum/vm/AddAlbumViewModelFactory.kt
 package com.zak.pressmark.feature.addalbum.vm
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.zak.pressmark.data.local.repository.ReleaseRepository
 import com.zak.pressmark.data.repository.AlbumRepository
 import com.zak.pressmark.data.repository.ArtistRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -9,6 +11,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 class AddAlbumViewModelFactory(
     private val albumRepository: AlbumRepository,
     private val artistRepository: ArtistRepository,
+    private val releaseRepository: ReleaseRepository,
 ) : ViewModelProvider.Factory {
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -18,6 +21,7 @@ class AddAlbumViewModelFactory(
             return AddAlbumViewModel(
                 albumRepository = albumRepository,
                 artistRepository = artistRepository,
+                releaseRepository = releaseRepository,
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
